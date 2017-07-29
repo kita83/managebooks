0bc6128266b2ee713d650e9132b601b8f435370b
strkita <kita83@gmail.com>
2017-07-29 16:51:34 +0900

    addTemplate

--- managebooks/settings.py
+++ managebooks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',  # django-bootstrap-form
     'cms',
 ]
 
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 静的ファイルを共通で置く
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
